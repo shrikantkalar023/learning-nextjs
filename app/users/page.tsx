16fbc0ed6da8a6a0c05a8ddc6e1e8c3dcdfca791
Shrikant Kalar <shrikantkalar023@gmail.com>
2024-05-08 16:14:36 +0530

    fetching data on server

--- app/users/page.tsx
+++ app/users/page.tsx
@@ -1,5 +1,22 @@
-const UsersPage = () => {
-  return <div>UsersPage</div>;
+interface User {
+  id: number;
+  name: string;
+}
+
+const UsersPage = async () => {
+  const res = await fetch("https://jsonplaceholder.typicode.com/users");
+  const users: User[] = await res.json();
+
+  return (
+    <>
+      <h1>Users</h1>
+      <ul>
+        {users.map((u) => (
+          <li key={u.id}>{u.name}</li>
+        ))}
+      </ul>
+    </>
+  );
 };
 
 export default UsersPage;
